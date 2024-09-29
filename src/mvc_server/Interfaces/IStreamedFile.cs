ca604761377e0b49072ae345c6fa65bbe1b50289
Tomishiko <anton20435@outlook.com>
2024-09-29 17:19:23 +0300

    add file handle wrapper

--- src/mvc_server/Interfaces/IStreamedFile.cs
+++ src/mvc_server/Interfaces/IStreamedFile.cs
@@ -1,17 +1,19 @@
 using System;
+using mvc_server.Interfaces;
 using Microsoft.Win32.SafeHandles;
 
 namespace mvc_server.Interfaces;
 
 public interface IStreamedFile
 {
-    string Id { get; init; }
-    long FileSize { get; init; }
-    int TotalFileParts { get; init; }
-    string FileName { get; init; }
-    long PartSize { get; init; }
-    SafeFileHandle Stream { get; init; }
-    DateTime Created { get; init; }
+    string Id { get; }
+    long FileSize { get; }
+    int TotalFileParts { get; }
+    string FileName { get; }
+    long PartSize { get; }
+    SafeFileHandle GetFileHandle { get; }
+    void Close();
+    DateTime Created { get; }
     event EventHandler<string>? CloseEvent;
     int PartsWritten { get; set; }
 
